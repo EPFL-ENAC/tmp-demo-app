c528e6a5613b405865673e6446f3e2d5bbb552a0
Samuel Bancal <Samuel.Bancal@epfl.ch>
2023-05-01 16:44:11 +0200

    feat(demo): go on!

--- hello-vue3/src/components/HelloWorld.vue
+++ hello-vue3/src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ defineProps<{
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
       <br />
-      Welcome to DEMO ! : )
+      DEMO is right now !
     </h3>
   </div>
 </template>
