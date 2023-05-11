5940835f2199ddaf6845f68357b5d254821df657
Samuel Bancal <Samuel.Bancal@epfl.ch>
2023-05-11 10:45:18 +0200

    feat: test CD 10:45

--- hello-vue3/src/components/HelloWorld.vue
+++ hello-vue3/src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ defineProps<{
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
       <br />
-      CD tested on 2023-05-11 10:14
+      CD tested on 2023-05-11 10:45
     </h3>
   </div>
 </template>
